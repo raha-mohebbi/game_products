6c82457fbbada4f5e5d80a1d45077598e14abcaf
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-04-19 20:39:53 +0330

    remove useless files

--- dashboard-app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import LoginPage from "./login/page";
-export default function Home() {
-  return (
-    <div className="">
-      <LoginPage />
-    </div>
-  );
-}
